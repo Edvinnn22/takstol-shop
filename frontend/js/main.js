71bf5ea3521d9f8bb076194314423d38efe53fe3
Edvinnn22 <edvin.hultqvist@icloud.com>
2026-04-18 02:54:04 +0200

    gitignore

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,8 +1,18 @@
+require('dotenv').config()
+
+console.log(process.env.API_KEY)
+
+
+
+
+// nav scroll
 const nav = document.querySelector('.nav');
   window.addEventListener('scroll', () => {
     nav.classList.toggle('nav--scrolled', window.scrollY > 20);
   }, { passive: true });
 
+
+  // hamburger
 const toggle = document.querySelector('.nav__toggle');
 const menu = document.querySelector('.nav__center');
 
